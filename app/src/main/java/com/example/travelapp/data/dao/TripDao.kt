8e303035a16310455010fbf567c49cb1bd6f48fb
lucatrianti <luca.trianti@mangomobi.com>
2025-08-17 16:34:46 +0200

    Added User and Trip entities for db

---
+++ app/src/main/java/com/example/travelapp/data/dao/TripDao.kt
@@ -0,0 +1,17 @@
+package com.example.travelapp.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.travelapp.data.model.Trip
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TripDao {
+
+    @Query("SELECT * FROM trips")
+    fun getAllTrips(): Flow<List<Trip>>
+
+    @Insert
+    suspend fun insertTrip(trip: Trip)
+}
---
+++ app/src/main/java/com/example/travelapp/data/dao/UserDao.kt
@@ -0,0 +1,13 @@
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM users")
+    fun getAllUsers(): Flow<List<User>>
+
+    @Insert
+    suspend fun insertUser(user: User)
+}
