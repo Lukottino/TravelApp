7a02028af634b5145eb77831767fe8d4ba0db8c2
Luca Trianti <luca.trianti@mangomobi.com>
2026-06-11 22:07:17 +0200

    Fixed: Phone top status bar was dark when in dark mode

--- app/src/main/java/com/example/travelapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/travelapp/ui/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.example.travelapp.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColors = darkColorScheme(
     primary = Purple80,
@@ -29,6 +33,14 @@ fun TravelAppTheme(
         else    -> isSystemInDarkTheme()
     }
     val colors = if (darkTheme) DarkColors else LightColors
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
 
     MaterialTheme(
         colorScheme = colors,
